3a4e5feeb863102de23d480af498892c49d76877
koresachin7 <koresachin7@gmail.com>
2021-09-13 23:42:21 +0530

    Added : Create Method allowPlayer in this mathod use Switch case to choise player

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,11 +1,21 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+    public static char playerSymbol;
+    public static char cpSymbol;
     static char[] board = new char[10]; // board size 10
     public static void main(String[] args) {
         createBoard();//calling method
-        System.out.printf("Welcome to Tic Tac Toe Game");
+        System.out.println("Select the Letter you wanted \n  Press 1 . Choose X \n  Press 2 . Choose O\n\nEnter your Choice : ");
+        Scanner scan =new Scanner(System.in); //Create scanner object
+        int choice = scan.nextInt();
+
+        TicTacToe.allowPlayer(choice);
+        scan.close();
     }
+
     /*Method use:
      *in method data is character
      *and using for loop to iteration from 1 to 9
@@ -14,6 +24,26 @@ public class TicTacToe {
     public static void createBoard() {
         for (int index = 1; index < board.length; index++) {
             board[index] = ' ';
+        }
+
+
+    }
+    public  static void allowPlayer( int choice){
+        switch(choice){
+            case 1 : {
+                playerSymbol='X';
+                cpSymbol='O';
+                break;
+            }
+            case 2 :{
+                playerSymbol='O';
+                cpSymbol='X';
+                break;
+            }
+            default: {
+                System.out.println("Invalid Choice . Retry Again!");
+                return;
+            }
         }
     }
 }

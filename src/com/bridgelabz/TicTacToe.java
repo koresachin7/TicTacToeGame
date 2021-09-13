61c09fa379cd3b1463d9474acf8e75bb7ca7247c
koresachin7 <koresachin7@gmail.com>
2021-09-13 23:35:09 +0530

    Added : Create a method called createBoard and iteration between 1 to 9

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,7 +1,19 @@
 package com.bridgelabz;
 
 public class TicTacToe {
+    static char[] board = new char[10]; // board size 10
     public static void main(String[] args) {
+        createBoard();//calling method
         System.out.printf("Welcome to Tic Tac Toe Game");
+    }
+    /*Method use:
+     *in method data is character
+     *and using for loop to iteration from 1 to 9
+
+     */
+    public static void createBoard() {
+        for (int index = 1; index < board.length; index++) {
+            board[index] = ' ';
+        }
     }
 }

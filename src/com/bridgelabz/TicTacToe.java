2e769b678bd354697ec2ff3eba4b4a75ec0d5e03
koresachin7 <koresachin7@gmail.com>
2021-09-14 00:04:32 +0530

    Added : Adding method to choices user to move

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -3,18 +3,19 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TicTacToe {
+    static Scanner scan =new Scanner(System.in); //Create scanner object class
     public static char playerSymbol;
     public static char cpSymbol;
     static char[] board = new char[10]; // board size 10
     public static void main(String[] args) {
         createBoard();//calling method
         System.out.println("Select the Letter you wanted \n  Press 1 . Choose X \n  Press 2 . Choose O\n\nEnter your Choice : ");
-        Scanner scan =new Scanner(System.in); //Create scanner object
         int choice = scan.nextInt();
 
 
         TicTacToe.allowPlayer(choice);
         TicTacToe.showBoard();
+        choicesLocation();
         scan.close();
     }
 
@@ -53,5 +54,59 @@ public class TicTacToe {
         System.out.println(" | "+board[1]+" | "+board[2]+" | "+board[3]+" |\n");
         System.out.println(" | "+board[4]+" | "+board[5]+" | "+board[6]+" |\n");
         System.out.println(" | "+board[7]+" | "+board[8]+" | "+board[9]+" |\n");
+    }
+    static void choicesLocation() {
+        System.out.println("Enter board index(1-9) to set your game move ");
+        int userMove = scan.nextInt();
+        switch (userMove) {
+            case 1:
+                if (board[1] == ' ') {
+                    board[1] = playerSymbol;
+                }
+                break;
+            case 2:
+                if (board[2] == ' ') {
+                    board[2] = playerSymbol;
+                }
+                break;
+            case 3:
+                if (board[3] == ' ') {
+                    board[3] = playerSymbol;
+                }
+                break;
+            case 4:
+                if (board[4] == ' ') {
+                    board[4] = playerSymbol;
+                }
+                break;
+            case 5:
+                if (board[5] == ' ') {
+                    board[5] = playerSymbol;
+                }
+                break;
+            case 6:
+                if (board[6] == ' ') {
+                    board[6] = playerSymbol;
+                }
+                break;
+            case 7:
+                if (board[7] == ' ') {
+                    board[7] = playerSymbol;
+                }
+                break;
+            case 8:
+                if (board[8] == ' ') {
+                    board[8] = playerSymbol;
+                }
+                break;
+            case 9:
+                if (board[9] == ' ') {
+                    board[9] = playerSymbol;
+                }
+                break;
+            default:
+                System.out.println("Invalid Move");
+        }
+        showBoard();
     }
 }
